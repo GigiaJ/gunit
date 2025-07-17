a260294fcae0b55acf85ac8f88be2f9dd3412c2b
Gigiaj <gigiaboone@yahoo.com>
2025-07-17 08:01:06 -0500

    add elf patch as native input

--- gchannel/packages/floorp.scm
+++ gchannel/packages/floorp.scm
@@ -237,7 +237,7 @@
                 (delete 'validate-runpath)
             )))
     (native-inputs
-        (list git))
+        (list git patchelf))
     (synopsis "Soup")
     (home-page "https://coder.com/")
     (description "Free open source code server")
