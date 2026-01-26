d44772d23165cff8bcf2f28d8ae7272e19dacd74
GigiaJ <gigiaboone@yahoo.com>
2026-01-25 21:26:30 -0600

    patched rocm-tools

--- modules/gunit/packages/rocm-tools.scm
+++ modules/gunit/packages/rocm-tools.scm
@@ -15,7 +15,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (amd packages rocm-tools)
+(define-module (gunit packages rocm-tools)
   #:use-module (guix packages)
   #:use-module (guix gexp)
   #:use-module (guix build-system cmake)
