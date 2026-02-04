adaa8ee49390bf3ec424ae25f260eb901ee198b9
GigiaJ <gigiaboone@yahoo.com>
2026-02-04 16:35:51 -0600

    haitus for ollama until ROCM merge

--- modules/gunit/packages/ai.scm
+++ modules/gunit/packages/ai.scm
@@ -1,3 +1,13 @@
+;; Currently non-functional in a practical sense
+;; Will detect the GPU but models fail to execute.
+;; Upstream progress on ROCm 7.1's math libraries
+;; ROCBLAS and HIPBLAS are in merge progress
+;; actively, so we should wait for that and try
+;; again with the 7.1 build which may address 
+;; some stability issues anyway. Doesn't hurt
+;; to have eyes belonging to smarter individuals
+;; than myself looking at it.
+;; https://codeberg.org/guix/guix/pulls/5787
 (define-module (gunit packages ai)
   #:use-module (guix)
   #:use-module ((guix licenses)
@@ -33,6 +43,10 @@
   #:use-module (gnu packages gl)
   #:use-module (gunit packages rocm-hip)
   #:use-module (gunit packages go-common))
+
+;;(define-public cuda-union
+;;  )
+
 
 (define-public rocm-union
   (package
@@ -105,7 +119,7 @@
 (define-public ollama-libs
   (package
     (name "ollama-libs")
-    (version "0.9.2")
+    (version "0.12.3")
     (source
      (origin
        (method git-fetch)
@@ -115,19 +129,23 @@
        (file-name (git-file-name "ollama" version))
        (modules '((guix build utils)))
        (sha256
-        (base32 "0bg2rqs9n9lgpv87fk22ragspgg4cy5cd29nr8vgr8zd8whdlsys"))))
+        (base32 "1d1x83056jnjahm2nw7m2i9mkalai5xn6h1rrkq9y615770wd052"))))
     (build-system cmake-build-system)
     (arguments
      (list
       #:build-type "Release"
       #:tests? #f
+      #:build-flags
+      #~(list (string-append
+               "-ldflags=-X github.com/ollama/ollama/server.RunnerDir="
+               #$output "/lib/ollama"))
       #:configure-flags
       #~(let ((rocm (assoc-ref %build-inputs "rocm-union")))
           (list "-DCMAKE_SKIP_BUILD_RPATH=ON"
                 "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON"
                 "-DAMDGPU_TARGETS=gfx1030"
-                "-DCMAKE_INSTALL_LIBDIR=lib/ollama"
-                "-DCMAKE_INSTALL_BINDIR=lib/ollama"
+                "-DCMAKE_INSTALL_LIBDIR=lib/ollama" ;; Can be removed for >15
+                ;;  "-DCMAKE_INSTALL_BINDIR=lib/ollama" ;; Can be removed for >15
                 (string-append "-DCMAKE_CXX_COMPILER="
                                #$rocm-union "/bin/hipcc")
                 (string-append "-DCMAKE_C_COMPILER="
@@ -142,10 +160,21 @@
                                #$vulkan-union "/lib/libvulkan.so")
                 (string-append "-DVulkan_INCLUDE_DIR="
                                #$vulkan-union "/include")
-
+                (string-append "-DVULKAN_SDK="
+                               #$vulkan-union)
+                (string-append "-DOLLAMA_RUNNER_DIR=" "rocm") ;; Likely needs to be dynamically set
                 (string-append "-DCMAKE_CXX_FLAGS=" "--rocm-device-lib-path="
                                #$rocm-union "/amdgcn/bitcode "
                                "-fpermissive -D_GNU_SOURCE")
+                (string-append "-DCMAKE_INSTALL_RPATH="
+                               #$rocm-union
+                               "/lib"
+                               ";"
+                               #$vulkan-union
+                               "/lib"
+                               ";"
+                               #$output
+                               "/lib/ollama")
                 (string-append "-DCMAKE_PREFIX_PATH="
                                #$rocm-union ";"
                                #$vulkan-union)))
@@ -164,17 +193,9 @@
 (define-public ollama
   (package
     (name "ollama")
-    (version "0.9.2")
+    (version "0.12.3")
     (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/ollama/ollama")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name "ollama" version))
-       (modules '((guix build utils)))
-       (sha256
-        (base32 "0bg2rqs9n9lgpv87fk22ragspgg4cy5cd29nr8vgr8zd8whdlsys"))))
+     (package-source ollama-libs))
     (build-system go-build-system)
     (arguments
      (list
@@ -185,20 +206,24 @@
       #:import-path "github.com/ollama/ollama"
       #:phases
       #~(modify-phases %standard-phases
-          (add-after 'install 'apply
+          (add-after 'install 'copy
             (lambda* (#:key outputs inputs #:allow-other-keys)
               (let* ((lib-ollama (string-append (assoc-ref outputs "out")
                                                 "/lib/ollama")))
                 (mkdir-p lib-ollama)
-                (for-each (lambda (file)
-                            (symlink file
-                                     (string-append lib-ollama "/"
-                                                    (basename file))))
-                          (find-files (string-append #$ollama-libs
-                                                     "/lib/ollama") "\\."))
-
-                (symlink (string-append (assoc-ref inputs "rocm-union") "/lib")
-                         (string-append lib-ollama "/rocm")))))
+                (copy-recursively (string-append #$ollama-libs "/lib/ollama")
+                                  lib-ollama
+                                  #:copy-file symlink))))
+          (add-after 'install 'wrap
+            (lambda* (#:key outputs inputs #:allow-other-keys)
+              (let* ((out (assoc-ref outputs "out"))
+                     (bin (string-append out "/bin/ollama"))
+                     (libs (string-append #$ollama-libs "/lib/ollama")))
+                (wrap-program bin
+                  `("OLLAMA_LIBRARY_PATH" ":" prefix
+                    (,libs))
+                  `("LD_LIBRARY_PATH" ":" prefix
+                    (,(string-append #$rocm-union "/lib")))))))
           (delete 'check)
           (delete 'strip)
           (delete 'validate-runpath))))
@@ -217,6 +242,7 @@
               ("go-golang-org-x-sys" ,go-golang-org-x-sys)
               ("go-golang-org-x-exp" ,go-golang-org-x-exp)
               ("go-golang-org-x-crypto" ,go-golang-org-x-crypto)
+              ("go-github-com-pkg-browser" ,go-github-com-pkg-browser)
               ("go-github-com-gin-contrib-cors" ,go-github-com-gin-contrib-cors)
               ("go-golang-org-x-tools" ,go-golang-org-x-tools)
               ("go-golang-org-x-image" ,go-golang-org-x-image)
@@ -224,6 +250,10 @@
               ("go-github-com-nlpodyssey-gopickle" ,go-github-com-nlpodyssey-gopickle)
               ("go-github-com-mattn-go-runewidth" ,go-github-com-mattn-go-runewidth)
               ("go-github-com-google-go-cmp" ,go-github-com-google-go-cmp)
+              ("go-github-com-mattn-go-sqlite3" ,go-github-com-mattn-go-sqlite3)
+              ("go-golang-org-x-sys" ,go-golang-org-x-sys)
+              ("go-github-com-tkrajina-typescriptify-golang-structs" ,go-github-com-tkrajina-typescriptify-golang-structs)
+              (" go-github-com-thetitanrain-w32" ,go-github-com-thetitanrain-w32)
               ("go-github-com-emirpasic-gods-v2" ,go-github-com-emirpasic-gods-v2)
               ("go-github-com-dlclark-regexp2-v1.11.5" ,go-github-com-dlclark-regexp2-v1.11.5)
               ("go-github-com-d4l3k-go-bfloat16" ,go-github-com-d4l3k-go-bfloat16)
