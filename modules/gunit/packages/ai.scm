f8e50a6fc657b73311bf98dde3f04896865cfcfc
GigiaJ <gigiaboone@yahoo.com>
2026-02-04 09:52:16 -0600

    Clean up the build back to our segmented form w/ a working version

--- modules/gunit/packages/ai.scm
+++ modules/gunit/packages/ai.scm
@@ -1,9 +1,3 @@
-;; I rewrote this literally over a dozen times
-;; I will rewrite it one more later, but
-;; at the current time this *does* 
-;; work for AMD GPUs (and with little)
-;; effort it can be cleaned up to work 
-;; all GPUs. Calling it a day though.
 (define-module (gunit packages ai)
   #:use-module (guix)
   #:use-module ((guix licenses)
@@ -121,7 +115,7 @@
        (file-name (git-file-name "ollama" version))
        (modules '((guix build utils)))
        (sha256
-        (base32 "149l7s99n7f3ys6hrxrcf4pq85cmpgxyj5d1vc720sr6cdajxwc5"))))
+        (base32 "0bg2rqs9n9lgpv87fk22ragspgg4cy5cd29nr8vgr8zd8whdlsys"))))
     (build-system cmake-build-system)
     (arguments
      (list
@@ -129,55 +123,36 @@
       #:tests? #f
       #:configure-flags
       #~(let ((rocm (assoc-ref %build-inputs "rocm-union")))
-          (list (string-append "-DCMAKE_CXX_COMPILER=" rocm "/bin/hipcc")
-                (string-append "-DCMAKE_C_COMPILER=" rocm "/bin/hipcc")
-                (string-append "-DCMAKE_HIP_COMPILER=" rocm "/bin/hipcc")
-                (string-append "-DCMAKE_CXX_FLAGS=" "--rocm-device-lib-path="
-                               rocm "/amdgcn/bitcode "
-                               "-fpermissive -D_GNU_SOURCE")
-                (string-append "-DCMAKE_PREFIX_PATH=" rocm ";"
-                               #$vulkan-union)
-                (string-append "-DCMAKE_INSTALL_RPATH="
-                               rocm
-                               ";"
-                               #$vulkan-tools
-                               ";"
-                               "$ORIGIN")
-                (string-append "-DVULKAN_SDK="
-                               #$vulkan-union)
+          (list "-DCMAKE_SKIP_BUILD_RPATH=ON"
+                "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON"
                 "-DAMDGPU_TARGETS=gfx1030"
-                ;;  "-DCMAKE_HIP_ARCHITECTURES=gfx1030;gfx1100"
                 "-DCMAKE_INSTALL_LIBDIR=lib/ollama"
                 "-DCMAKE_INSTALL_BINDIR=lib/ollama"
-                "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON"
-                "-DCMAKE_SKIP_BUILD_RPATH=ON"
-                "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON"
-                "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON"))
+                (string-append "-DCMAKE_CXX_COMPILER="
+                               #$rocm-union "/bin/hipcc")
+                (string-append "-DCMAKE_C_COMPILER="
+                               #$rocm-union "/bin/hipcc")
+                (string-append "-DCMAKE_HIP_COMPILER="
+                               #$rocm-union "/bin/hipcc")
+                (string-append "-DROCM_PATH="
+                               #$rocm-union)
+                (string-append "-DHIP_PATH="
+                               #$rocm-union)
+                (string-append "-DVulkan_LIBRARY="
+                               #$vulkan-union "/lib/libvulkan.so")
+                (string-append "-DVulkan_INCLUDE_DIR="
+                               #$vulkan-union "/include")
+
+                (string-append "-DCMAKE_CXX_FLAGS=" "--rocm-device-lib-path="
+                               #$rocm-union "/amdgcn/bitcode "
+                               "-fpermissive -D_GNU_SOURCE")
+                (string-append "-DCMAKE_PREFIX_PATH="
+                               #$rocm-union ";"
+                               #$vulkan-union)))
       #:phases
       #~(modify-phases %standard-phases
           (delete 'check)
-          (replace 'build
-            (lambda* (#:key inputs #:allow-other-keys)
-              (setenv "CGO_ENABLED" "1")
-              (setenv "CGO_CFLAGS"
-                      (string-append "-I"
-                                     #$(package-source ollama-libs)
-                                     "/ml/backend/ggml/ggml/include"))
-              (setenv "CGO_LDFLAGS"
-                      (string-append "-L"
-                                     (assoc-ref inputs "ollama-libs")
-                                     "/lib/ollama"))
-
-              (invoke "go"
-                      "build"
-                      "-tags"
-                      "rocm vulkan"
-                      "-ldflags"
-                      "-X github.com/ollama/ollama/gpu.PayloadLibDir="
-                      "github.com/ollama/ollama"
-                      "-o"
-                      "ollama")))
-          ;;          (delete 'validate-runpath)
+          (delete 'validate-runpath)
           (delete 'strip))))
     (native-inputs (list cmake pkg-config perl bash))
     (inputs (list rocm-union vulkan-union glslang))
@@ -210,89 +185,17 @@
       #:import-path "github.com/ollama/ollama"
       #:phases
       #~(modify-phases %standard-phases
-          (add-before 'build 'cmake
+          (add-after 'install 'apply
             (lambda* (#:key outputs inputs #:allow-other-keys)
-              (begin
-                (chdir "src/github.com/ollama/ollama")
-
-                (system (string-join `("cmake -B build"
-
-                                       "-DCMAKE_SKIP_BUILD_RPATH=ON"
-                                       "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON"
-                                       "-DAMDGPU_TARGETS=gfx1030"
-                                       ,(string-append "-DCMAKE_CXX_COMPILER="
-                                         #$rocm-union "/bin/hipcc")
-                                       ,(string-append "-DCMAKE_C_COMPILER="
-                                                       #$rocm-union
-                                                       "/bin/hipcc")
-                                       ,(string-append "-DCMAKE_HIP_COMPILER="
-                                         #$rocm-union "/bin/hipcc")
-                                       ,(string-append "-DROCM_PATH="
-                                                       #$rocm-union)
-                                       ,(string-append "-DHIP_PATH="
-                                                       #$rocm-union)
-                                       ,(string-append "-DVulkan_LIBRARY="
-                                                       #$vulkan-union
-                                                       "/lib/libvulkan.so")
-                                       ,(string-append "-DVulkan_INCLUDE_DIR="
-                                         #$vulkan-union "/include")
-
-                                       ,(string-append "-DCMAKE_CXX_FLAGS=\""
-                                         "--rocm-device-lib-path="
-                                         #$rocm-union "/amdgcn/bitcode "
-                                         "-fpermissive -D_GNU_SOURCE\"")
-                                       ,(string-append
-                                         "-DCMAKE_PREFIX_PATH=\""
-                                         #$rocm-union ";"
-                                         #$vulkan-union "\"")) " "))
-                (system (string-append "cmake --build build -j"
-                                       (number->string (parallel-job-count))))
-                (let ((out-lib (string-append #$output "/lib")))
-                  (mkdir-p out-lib)
-                  (copy-recursively "build/lib" out-lib))
-                (let ((out-lib (string-append ".")))
-                  (copy-recursively "build/lib/ollama" out-lib))
-                (symlink (string-append #$rocm-union "/lib")
-                         (string-append "build/lib/ollama/rocm")))))
-          (replace 'build
-            (lambda* (#:key inputs #:allow-other-keys)
-              (setenv "CGO_ENABLED" "1")
-
-              (let ((root (getcwd)))
-                (setenv "CGO_CFLAGS"
-                        (string-append "-I" root
-                                       "/ml/backend/ggml/ggml/include"))
-                (setenv "CGO_LDFLAGS"
-                        (string-append "-L" root "/ml/backend/ggml/ggml/src")))
-              (invoke "go"
-                      "build"
-                      "-tags"
-                      "rocm vulkan"
-                      "-ldflags"
-                      "-X=github.com/ollama/ollama/version.Version=0.15.2"
-                      "-o"
-                      "ollama"
-                      ".")
-              (install-file "ollama" "../../../..")
-              (chdir "../../../..")))
-
-          (replace 'install
-            (lambda* (#:key outputs inputs #:allow-other-keys)
-              (let* ((out (assoc-ref outputs "out"))
-                     (bin (string-append out "/bin"))
-                     (lib-ollama (string-append out "/lib/ollama"))
-                     (src-path "src/github.com/ollama/ollama"))
-                
-                (mkdir-p bin)
+              (let* ((lib-ollama (string-append (assoc-ref outputs "out")
+                                                "/lib/ollama")))
                 (mkdir-p lib-ollama)
-
-                (install-file (string-append src-path "/ollama") bin)
-
-                (for-each (lambda (f)
-                            (install-file f lib-ollama))
-                          (find-files (string-append src-path
-                                                     "/build/lib/ollama")
-                                      "\\.so$"))
+                (for-each (lambda (file)
+                            (symlink file
+                                     (string-append lib-ollama "/"
+                                                    (basename file))))
+                          (find-files (string-append #$ollama-libs
+                                                     "/lib/ollama") "\\."))
 
                 (symlink (string-append (assoc-ref inputs "rocm-union") "/lib")
                          (string-append lib-ollama "/rocm")))))
@@ -301,7 +204,8 @@
           (delete 'validate-runpath))))
 
     (native-inputs (list pkg-config patchelf cmake))
-    (inputs `(("rocm-union" ,rocm-union)
+    (inputs `(("ollama-libs" ,ollama-libs)
+              ("rocm-union" ,rocm-union)
               ("vulkan-loader" ,vulkan-loader)
               ("libdrm" ,libdrm)
               ("gcc:lib" ,gcc)
