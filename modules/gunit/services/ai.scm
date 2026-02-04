3a55229efac6a6747afbdd648dc8ed670cf7b789
GigiaJ <gigiaboone@yahoo.com>
2026-02-04 11:19:46 -0600

    fix our service decl

--- modules/gunit/services/ai.scm
+++ modules/gunit/services/ai.scm
@@ -5,6 +5,7 @@
   #:use-module (gnu home services)
   #:use-module (gnu home services shepherd)
   #:use-module (guix packages)
+  #:use-module (gnu packages)
   #:export (home-ollama-service-type))
 
 (define (ollama-home-service config)
