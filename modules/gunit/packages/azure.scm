a27abcadd9810a0ad131f8618e5a74056c70c022
GigiaJ <gigiaboone@yahoo.com>
2026-02-06 13:14:53 -0600

    update azure CLI to run much more cleanly

--- modules/gunit/packages/azure.scm
+++ modules/gunit/packages/azure.scm
@@ -17,7 +17,7 @@
   #:use-module (nonguix multiarch-container)
   #:use-module (nonguix utils))
 
-(define-public az
+(define-public azure-cli
   (package
     (name "azure-cli")
     (version "2.82.0-1")
@@ -39,84 +39,56 @@
         (base32 "045mx5j2hhrbh54brg6w2z9v59x3yzapq474k2iz2f3virfkhchz"))))
     (build-system gnu-build-system)
     (arguments
-     
-     '(#:tests? #f
-       #:validate-runpath? #f
-       #:phases
-
-       (modify-phases %standard-phases
-         (replace 'unpack
-           (lambda* (#:key source #:allow-other-keys)
-             (invoke "ar" "x" source)
-             (invoke "tar" "xf" "data.tar.xz")))
-         (delete 'configure)
-         (delete 'build)
-         (replace 'install
-           (lambda* (#:key outputs #:allow-other-keys)
-             (let* ((out (assoc-ref outputs "out"))
-                    (bin (string-append out "/bin")))
-               (copy-recursively "opt"
-                                 (string-append out "/opt"))
-               (copy-recursively "usr"
-                                 (string-append out "/usr"))
-               (mkdir-p bin))))
-         (add-after 'install 'write-az-wrapper
-           (lambda* (#:key outputs #:allow-other-keys)
-             (let* ((out (assoc-ref outputs "out"))
-                    (bin (string-append out "/bin"))
-                    (py (string-append out "/opt/az/bin/python3")))
-               (mkdir-p bin)
-               (call-with-output-file (string-append bin "/az")
-                 (lambda (port)
-                   (format port "#!/bin/sh\nexec ~a -m azure.cli \"$@\"\n" py)))
-               (chmod (string-append bin "/az") #o755))))
-
-         (add-after 'install 'patch-python
-           (lambda* (#:key outputs inputs #:allow-other-keys)
-             (let* ((out (assoc-ref outputs "out"))
-                    (glibc (assoc-ref inputs "glibc"))
-                    (python (string-append out "/opt/az/bin/python3")))
-               (invoke "patchelf" "--set-interpreter"
-                       (string-append glibc "/lib/ld-linux-x86-64.so.2")
-                       python))))
-
-         )))
-    (inputs (list glibc))
+     (list
+      #:tests? #f
+      #:validate-runpath? #f
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'unpack
+            (lambda* (#:key source #:allow-other-keys)
+              (invoke "ar" "x" source)
+              (invoke "tar" "xf" "data.tar.xz")))
+          (delete 'configure)
+          (delete 'build)
+          (replace 'install
+            (lambda* (#:key outputs inputs #:allow-other-keys)
+              (let* ((out (assoc-ref outputs "out"))
+                     (bin (string-append out "/bin"))
+                     (py (string-append out "/opt/az/bin/python3"))
+                     (lib-path (string-join (map (lambda (name)
+                                                   (string-append (assoc-ref
+                                                                   inputs name)
+                                                                  "/lib"))
+                                                 '("zlib" "openssl" "libffi"
+                                                   "util-linux" "gcc-lib"))
+                                            ":")))
+                (copy-recursively "opt"
+                                  (string-append out "/opt"))
+                (copy-recursively "usr"
+                                  (string-append out "/usr"))
+                (invoke "patchelf" "--set-interpreter"
+                        (string-append (assoc-ref inputs "glibc")
+                                       "/lib/ld-linux-x86-64.so.2") py)
+                (mkdir-p bin)
+                (with-output-to-file (string-append bin "/az")
+                  (lambda ()
+                    (format #t
+                            ;; I think my formatter is too opinionated
+                            (string-append "#!/bin/sh\n"
+                             "export LD_LIBRARY_PATH=~a:$LD_LIBRARY_PATH" "\n"
+                             "exec ~a -W ignore -m azure.cli \"$@\"\n")
+                            lib-path py)))
+                (chmod (string-append bin "/az") #o755)))))))
+    (inputs `(("glibc" ,(@@ (gnu packages base) glibc-for-fhs))
+              ("zlib" ,zlib)
+              ("openssl" ,openssl)
+              ("libffi" ,libffi)
+              ("util-linux" ,util-linux)
+              ("gcc-lib" ,gcc "lib")))
     (native-inputs (list binutils tar patchelf))
     (synopsis "Azure CLI")
-    (description
-     "Azure CLI is a CLI tool for interacting with the Azure cloud platform.")
+    (description "Condensed FHS-compatible Azure CLI for Guix.")
     (home-page "https://github.com/Azure/azure-cli")
     (license license:asl2.0)))
 
-(define azure-cli-libs
-  `(("coreutils" ,coreutils)
-    ("zlib" ,zlib)
-    ("openssl" ,openssl)
-    ("libffi" ,libffi)
-    ("util-linux" ,util-linux)
-    ("gcc" ,gcc "lib")))
-
-(define azure-cli-ld.so.conf
-  (packages->ld.so.conf (list (fhs-union `(,@azure-cli-libs ,@fhs-min-libs)
-                                         #:name "fhs-union-64"))))
-
-(define azure-cli-ld.so.cache
-  (ld.so.conf->ld.so.cache azure-cli-ld.so.conf))
-
-(define-public azure-cli-container
-  (nonguix-container (name "az")
-                     (wrap-package az)
-                     (run "/bin/az")
-                     (ld.so.conf azure-cli-ld.so.conf)
-                     (ld.so.cache azure-cli-ld.so.cache)
-                     ;; TODO: This should almost certainly be 
-                     ;; shared with the actual user home
-                     ;; so it creates files in expected locations   
-                     (union64 (fhs-union `(,@azure-cli-libs ,@fhs-min-libs)
-                                         #:name "fhs-union-64"))
-                     (description (package-description az))))
-
-(define-public azure-cli
-  (nonguix-container->package azure-cli-container))
-
+azure-cli
